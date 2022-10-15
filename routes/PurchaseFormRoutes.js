92a435d55ee1ed7546e88380ce4c3c40b59d3365
Muhammad Hasnat Saghir <hasnatsaghir@gmail.com>
2022-10-15 12:33:01 +0500

    make it better

--- routes/PurchaseFormRoutes.js
+++ routes/PurchaseFormRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express"),
 	router = express.Router(),
-	controller = require("../controllers/PurchaseFormController");
+	controller = require("../controllers/purchaseFormController");
 const { auth } = require("../middleware/auth")
 
 router.post("/post", controller.postPurchaseForm);
